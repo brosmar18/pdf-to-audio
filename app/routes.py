5d50a10b06edf3ea308f5d4bb7a39609db273f2f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 18:11:59 -0500

    handle post request

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,13 @@
-from flask import render_template
+from flask import render_template, request
 from app import app
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        pdf_file = request.files['pdf_file']
+        if pdf_file:
+            # print name of uploaded file.
+            print(f"Uploaded file: {pdf_file.filename}")
+
+
     return render_template('index.html')
