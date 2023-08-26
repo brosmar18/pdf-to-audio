d27489ec654d2b600c36cb2a57f4659e64609ab6
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 18:40:34 -0500

    imports added

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,15 @@
 from flask import render_template, request
 from app import app
+import PyPDF2
+from gtts import gTTS
+import os
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         pdf_file = request.files['pdf_file']
         if pdf_file:
-            # For now, let's just print the name of the uploaded file
-            print(f"Uploaded file: {pdf_file.filename}")
+           # Read PDF
+
 
     return render_template('index.html')
