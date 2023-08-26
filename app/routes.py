5961d7867dfbeda569fe131d300ab320536a92bc
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 18:38:50 -0500

    bugs fixed

--- app/routes.py
+++ app/routes.py
@@ -6,8 +6,7 @@ def index():
     if request.method == 'POST':
         pdf_file = request.files['pdf_file']
         if pdf_file:
-            # print name of uploaded file.
+            # For now, let's just print the name of the uploaded file
             print(f"Uploaded file: {pdf_file.filename}")
-
 
     return render_template('index.html')
