b157fa68ceb05b1dce9248fa3bcc44a8eb5b73e3
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 18:47:59 -0500

    convert pdf to audio

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, send_file
 from app import app
 import PyPDF2
 from gtts import gTTS
@@ -14,5 +14,13 @@ def index():
             text = ""
             for page in pdf_reader.pages:
                 text += page.extract_text()
+
+            # Convert text to audio
+            tts = gTTS(text)
+            audio_file_path = os.path.join("static", "output.mp3")
+            tts.save(audio_file_path)
+
+             # Return audio file
+            return send_file(audio_file_path, as_attachment=True, download_name="output.mp3")
 
     return render_template('index.html')
