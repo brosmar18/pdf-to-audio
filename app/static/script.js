69db24bc00d1fa2ba4481c14a82aad324b58b0a7
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 20:05:39 -0500

    js updated to add spinner

--- app/static/script.js
+++ app/static/script.js
@@ -2,12 +2,19 @@ console.log("file connected");
 
 document.addEventListener('DOMContentLoaded', function () {
     const pdfInput = document.querySelector('.converter__input');
+    const progressSpinner = document.getElementById('progressSpinner');
 
     pdfInput.addEventListener('change', function (event) {
+        progressSpinner.style.display = 'block';
         const file = event.target.files[0];
         if (file.type === 'application/pdf') {
             const pdfPreview = document.getElementById('pdfPreview');
             pdfPreview.innerHTML = `<embed src="${URL.createObjectURL(file)}" type="application/pdf" width="100%" height="600px">`;
         }
+
+        // Simulate conversion delay 
+        setTimeout(() => {
+            progressSpinner.style.display = "none"; // Hide spinner
+        }, 2000); // simulated delay in milliseconds
     });
 });
