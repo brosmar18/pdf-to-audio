db813d3f324db4d87b2f71888b6291d27ffbe93f
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 19:34:04 -0500

    JS added to preview pdf

---
+++ app/static/script.js
@@ -0,0 +1,13 @@
+console.log("file connected");
+
+document.addEventListener('DOMContentLoaded', function () {
+    const pdfInput = document.querySelector('.converter__input');
+
+    pdfInput.addEventListener('change', function (event) {
+        const file = event.target.files[0];
+        if (file.type === 'application/pdf') {
+            const pdfPreview = document.getElementById('pdfPreview');
+            pdfPreview.innerHTML = `<embed src="${URL.createObjectURL(file)}" type="application/pdf" width="100%" height="600px">`;
+        }
+    });
+});
