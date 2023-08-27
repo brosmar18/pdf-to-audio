8f58c0fd8816540197f05d5675d565b172730b8d
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-08-26 19:08:04 -0500

    file uploaded to converted to mp3 successfully

--- app/routes.py
+++ app/routes.py
@@ -6,21 +6,27 @@ import os
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    download_link = None  # Initialize the download link
     if request.method == 'POST':
         pdf_file = request.files['pdf_file']
         if pdf_file:
-           # Read PDF
+            # Read PDF
             pdf_reader = PyPDF2.PdfReader(pdf_file)
             text = ""
             for page in pdf_reader.pages:
                 text += page.extract_text()
 
             # Convert text to audio
+            # Specify the absolute or relative path to your static directory
+            static_dir = os.path.join(os.getcwd(), 'app', 'static')  
+            audio_file_path = os.path.join(static_dir, 'output.mp3')
             tts = gTTS(text)
-            audio_file_path = os.path.join("static", "output.mp3")
             tts.save(audio_file_path)
 
-             # Return audio file
-            return send_file(audio_file_path, as_attachment=True, download_name="output.mp3")
+            print("File uploaded and processed")  # Debug print
 
-    return render_template('index.html')
+            # Create a download link
+            download_link = f"/static/output.mp3"
+            print(f"Download link: {download_link}")  # Debug print
+
+    return render_template('index.html', download_link=download_link)
